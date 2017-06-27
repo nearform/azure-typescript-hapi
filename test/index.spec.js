846951829fab61a3fbdd96ea9979df46cb494b0b
Jan-Jan <jan.jan.code@gmail.com>
2017-06-27 12:45:09 +0200

    improved naming

---
+++ test/index.spec.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const Code = require('code')
+const Lab = require('lab')
+const Hapi = require('hapi')
+
+const lab = exports.lab = Lab.script()
+const describe = lab.describe
+const it = lab.it
+const expect = Code.expect
+
+lab.experiment('lab', function () {
+  lab.test('exists', function (done) {
+    Code.expect(lab).to.exist()
+    done()
+  })
+
+  lab.test('has properties', function (done) {
+    Code.expect(Object.keys(lab)).to.not.have.length(0)
+    done()
+  })
+})
